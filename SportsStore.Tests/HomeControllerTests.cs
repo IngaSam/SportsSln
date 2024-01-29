1afb46e75e883546e17556704684ecbc0f026557
IngaSam <kayumovainga2@mail.ru>
2024-01-29 21:16:49 +0300

    Подготовка контроллера

---
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            //Организация
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID =1, Name="P1"},
+                new Product {ProductID =2, Name="P2"}
+            }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            //Действие
+            IEnumerable<Product> result= (controller.Index() as ViewResult).ViewData.Model as IEnumerable<Product>;
+
+            //Утверждение
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length==2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+    }
+}
