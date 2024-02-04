646082ab0eb941ae66fca2b9598abfbf4e3cd207
IngaSam <kayumovainga2@mail.ru>
2024-02-04 18:23:37 +0300

    Добавление данных модели представления. ProductsListViewModel.cs

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -7,11 +7,38 @@ using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
 using Xunit;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Tests
 {
     public class HomeControllerTests
     {
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //Организация
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID=1, Name="P1"},
+                new Product {ProductID=2, Name="P2"},
+                new Product {ProductID=3, Name="P3"},
+                new Product {ProductID=4, Name="P4"},
+                new Product {ProductID=5, Name="P5"}
+            }).AsQueryable<Product>());
+            //Организация
+            HomeController controller= new HomeController(mock.Object)
+            { PageSize = 3};
+            //Действие 
+            ProductsListViewModel result = controller.Index(2).ViewData.Model as ProductsListViewModel;
+            //Утверждение
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
         [Fact]
         public void Can_Use_Repository()
         {
@@ -24,10 +51,11 @@ namespace SportsStore.Tests
             }).AsQueryable<Product>());
             HomeController controller = new HomeController(mock.Object);
             //Действие
-            IEnumerable<Product> result= (controller.Index() as ViewResult).ViewData.Model as IEnumerable<Product>;
+
+            ProductsListViewModel result= controller.Index().ViewData.Model as ProductsListViewModel;
 
             //Утверждение
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length==2);
             Assert.Equal("P1", prodArray[0].Name);
             Assert.Equal("P2", prodArray[1].Name);
@@ -49,10 +77,10 @@ namespace SportsStore.Tests
             controller.PageSize = 3;
 
             //Действие 
-            IEnumerable<Product> result = (controller.Index(2) as ViewResult).ViewData.Model as IEnumerable<Product>;
+            ProductsListViewModel result = controller.Index(2).ViewData.Model as ProductsListViewModel;
 
             //Утверждение 
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
