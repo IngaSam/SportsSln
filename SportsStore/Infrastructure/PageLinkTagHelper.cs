595d3e98886d63488d14f38c04b7925e6d28bdb4
IngaSam <kayumovainga2@mail.ru>
2024-02-11 20:24:24 +0300

    Исправление ошибки page

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -35,7 +35,7 @@ namespace SportsStore.Infrastructure
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
-                PageUrlValues["productPagek"] = i;
+                PageUrlValues["productPage"] = i;
                 tag.Attributes["href"]=urlHelper.Action(PageAction, PageUrlValues);
                 if (PageClassesEnabled)
                 {
