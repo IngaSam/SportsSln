fbce39e959904ceaefa25c4aeddada2b7c54459f
IngaSam <kayumovainga2@mail.ru>
2024-02-11 09:08:59 +0300

    Подсветка текущей категории

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -37,5 +37,31 @@ namespace SportsStore.Tests
             //Утверждение
             Assert.True(Enumerable.SequenceEqual(new string[] {"Apples", "Oranges", "Plums"},results));
         }
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            //Организация
+            string categoryToSelect = "Apples";
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product{ProductID=1, Name="P1", Category="Apples"},
+                new Product{ProductID=4, Name="P2", Category="Oranges"},
+
+            }).AsQueryable<Product>());
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+                }
+            };
+            target.RouteData.Values["category"]=categoryToSelect;
+            //Действие
+            string result = (string)(target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+            //Утверждение 
+            Assert.Equal(categoryToSelect, result);
+        }
     }
 }
